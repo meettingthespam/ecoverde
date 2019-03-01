bbecf26d1450dd9c9de7391dfd2ec9448c17cba1
john <34731198+meettingthespam@users.noreply.github.com>
2019-03-01 11:01:59 -0500

    Update config.py

--- FlaskBuild/config.py
+++ FlaskBuild/config.py
@@ -6,12 +6,12 @@ with open('/etc/config.json') as config_file:
 
 class Config:
 
-    'SECRET_KEY' = config.get('SECRET_KEY')
+    SECRET_KEY = config.get('SECRET_KEY')
 
-    'SQLALCHEMY_DATABASE_URI' = config.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     # using GMAIL for the sending and receiving of email
-    'MAIL_SERVER' = 'smtp.googlemail.com'
-    'MAIL_PORT' = 587
-    'MAIL_USE_TLS' = True
-    'MAIL_USERNAME' = config.get('EMAIL_USERNAME')
-    'MAIL_PASSWORD' = config.get('EMAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = config.get('EMAIL_USERNAME')
+    MAIL_PASSWORD = config.get('EMAIL_PASSWORD')
