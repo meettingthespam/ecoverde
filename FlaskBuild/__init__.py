6fbe63e781190fb696188b42b1a72202a2edac6f
john <34731198+meettingthespam@users.noreply.github.com>
2019-03-04 11:52:58 -0500

    Update __init__.py

--- FlaskBuild/__init__.py
+++ FlaskBuild/__init__.py
@@ -1,15 +1,11 @@
-# https://code.tutsplus.com/tutorials/intro-to-flask-adding-a-contact-page--net-28982
-# you're someweher ^^
-# and someweher at cory schafers tutorial nuimber 10
-
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from FlaskBuild.config import Config
 
 
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 mail = Mail()
 
 
@@ -18,7 +14,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    db.init_app(app)
+    # db.init_app(app)
     mail.init_app(app)
 
     from FlaskBuild.main.routes import main
